3f37b7d619a718ca3224feb853c56ff2ce905b6f
Petr Chromec <petr.chromec@lmc.eu>
2016-04-05 19:41:56 +0200

    twig added

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('Resources/views');
+$twig = new Twig_Environment($loader);
+
+echo $twig->render('index.html.twig', ['name' => 'world']);
