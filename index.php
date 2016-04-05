d8280210ddefcc51d4043ea140e7fa954fe43d47
Petr Chromec <petr.chromec@lmc.eu>
2016-04-06 00:11:27 +0200

    basic layout

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem('Resources/views');
 $twig = new Twig_Environment($loader);
 
-echo $twig->render('index.html.twig', ['name' => 'world']);
+echo $twig->render('index.html.twig');
